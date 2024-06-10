646b7dbe32b69fff9f6c024f52be7c83317d7e6d
sigalshemer1 <sigal.shemer68@gmail.com>
2024-06-10 15:29:22 -0400

    message

--- bookSystem/book_system.js
+++ bookSystem/book_system.js
@@ -26,7 +26,8 @@ function showbooks() {
         <p><strong>Book Name: </strong>${book.name}</p>
         <p><strong>Author Name:</strong> ${book.authorName}</p>
         <p><strong>Book Description:</strong> ${book.bookDescription}</p>
-        <p><strong>No. of Pages:</strong> ${book.pagesNumber} page(s)</p>`
+        <p><strong>No. of Pages:</strong> ${book.pagesNumber} page(s)</p><br>
+        <button onclick="deleteBtton(${index})">Delete</button>`
     );
     document.getElementById('books').innerHTML = booksDiv.join('');
 }
@@ -37,4 +38,9 @@ function showbooks() {
             document.getElementById('authorName').value = '';
             document.getElementById('bookDescription').value = '';
             document.getElementById('pagesNumber').value = '';
+ }
+
+ function deleteBtton(index){
+        books.splice(books[index],1);
+        showbooks()
  }
